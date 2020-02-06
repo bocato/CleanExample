4585da9fbce892e62c369184c97d006c05d604d9
Eduardo Sanches Bocato <dubocato@hotmart.com>
2020-02-06 11:56:04 -0300

    Initial Commit

---
+++ CleanExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanExample
+//
+//  Created by Eduardo Bocato on 06/02/20.
+//  Copyright © 2020 Bocato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
